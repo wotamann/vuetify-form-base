4f57549633c7fa635c449d95432fd1375777cb35
wotamann <woko@aon.at>
2021-02-22 17:55:01 +0100

    umd build

--- example/scripts/copy.js
+++ example/scripts/copy.js
@@ -7,7 +7,7 @@ const doTheJob = async() => {
   dir = '../dist/src';
   await fse.ensureDir(dir)
 
-  dir = '../../../../../wotamann.github.io/static';
+  dir = '../../../../../wotamann.github.io/vuetify/static';
   await fse.ensureDir(dir)
   console.log(`fse.ensureDir(dir) ### finished ###`);
 
@@ -18,11 +18,11 @@ const doTheJob = async() => {
   console.log(`fse.copy('./src/components/vFormBase.vue', '../dist/src/vFormBase.vue') ### finished ###`);
   
   
-  await fse.copy('../../../../../wotamann.github.io/static/index.html', '../../../../../wotamann.github.io/static/404.html');
-  console.log(`fse.copy('../../../../../wotamann.github.io/static/index.html', '../../../../../wotamann.github.io/static/404.html') ### finished ###`);
+  await fse.copy('../../../../../wotamann.github.io/vuetify/static/index.html', '../../../../../wotamann.github.io/vuetify/static/404.html');
+  console.log(`fse.copy('../../../../../wotamann.github.io/vuetify/static/index.html', '../../../../../wotamann.github.io/vuetify/static/404.html') ### finished ###`);
   
-  dir = `../../../../../wotamann.github.io/static`;
-  const destDir = `../../../../../wotamann.github.io`;
+  dir = `../../../../../wotamann.github.io/vuetify/static`;
+  const destDir = `../../../../../wotamann.github.io/vuetify`;
   // copy static folder to root
   await fse.copy(dir, destDir);
   console.log(`fse.copy(srcDir/static, destDir) ### finished ###`);
